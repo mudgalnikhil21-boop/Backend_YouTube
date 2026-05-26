39716e0984f2f4950bfb5d7633e2bb9ef6a0dfbd
Nikhil Mudgal <your_email@example.com>
2026-05-27 01:15:46 +0530

    add user routes for account management and history retrieval

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,5 +1,5 @@
 import Router from "express";
-import {registerUser, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controller.js";
+import {registerUser, loginUser, logoutUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import {upload} from  "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -30,5 +30,30 @@ router.route("/logout").post(
 )
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-passwrd").post(verifyJWT, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+router.route("/avatar").patch(
+    verifyJWT,
+    upload.single("avatar"),
+    updateUserAvatar
+)
+
+router.route("/cover-image").patch(
+    verifyJWT,
+    upload.single("coverImage"),
+    updateUserCoverImage
+)
+
+router.route("/c/:username").get(verifyJWT,
+    getUserChannelProfile
+)
+
+router.route("/history").get(verifyJWT, getWatchHistory)
+
 
 export default router;
